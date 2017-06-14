81d985d45169a0fde702ee04d580ff450cd5ecc2
Robert Doering <robert.w.doering@vanderbilt.edu>
2017-06-13 23:40:03 -0700

    Moved to .\Raw Data

--- mp3ToWav.py
+++
@@ -1,9 +0,0 @@
-import glob
-import subprocess
-import os.path
- 
-for fileName in glob.glob('.\mp3\*'):
-    print(fileName)
-    wavName =  ('.\wav\\' + fileName[6:-3] + 'wav')
-    if !(os.path.isfile(wavName)):
-        subprocess.call(['ffmpeg', '-i', fileName, wavName])
